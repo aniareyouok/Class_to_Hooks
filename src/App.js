cf745f84ee4edec66d12a841ff2465199840ff1b
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-22 00:54:12 +0100

    adding commits to class component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,35 @@ import './App.css';
 function App() {
   return (
     <div>
-      Schrijf hier jouw functie-component!
+        //creating component with state isLightOn: true
+        class App extends React.Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+        isLightOn: true
+    };
+        //binding function to component
+        this.handleClick = this.handleClick.bind(this);
+    }
+        //function sets state to opposite of current state
+        handleClick() {
+        this.setState({
+        isLightOn: !this.state.isLightOn,
+    });
+    }
+        //puts a html button with event onClick that invokes function handleClick and shows AAN or UIT depending on state
+        render() {
+        return (
+        <button
+        type="button"
+        onClick={this.handleClick}
+        >
+    {this.state.isLightOn ? 'AAN' : 'UIT'}
+        </button>
+        );
+    }
+    }
+
     </div>
   );
 }
